bc204838242298fe8ca6a963735cfd69f2c90e57
Navani-cpu <doolocompany@gmail.com>
2022-10-28 13:23:57 +0530

    fix:  for init firebase app

---
+++ public/firebase-sw.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDOooVxp3QF-4XtQIJw1Y6i-4Fp2gRokcc",
+  authDomain: "totalqsr-pwa-poc.firebaseapp.com",
+  projectId: "totalqsr-pwa-poc",
+  storageBucket: "totalqsr-pwa-poc.appspot.com",
+  messagingSenderId: "925807157827",
+  appId: "1:925807157827:web:f917db4a5bd48af40caaf6",
+  measurementId: "G-0YMTH62CG5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
